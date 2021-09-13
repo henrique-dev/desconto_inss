ef1b85f3be4b63d2570c01eea615a009671662c2
PH <henrique.phgb@gmail.com>
2021-09-13 12:27:21 -0300

    Rubocop fix

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -6,29 +6,28 @@ module ApplicationCable
       if cookies.signed[:admin_id]
         self.current_admin = find_verified_admin(cookies.signed[:admin_id])
         logger.add_tags 'ActionCable', current_admin.id
-      elsif request.headers["src"]
-        access_token = request.headers["access-token"]
-        uid = request.headers["uid"]
-        client = request.headers["client"]
-        if request.headers["src"] == "patient"
+      elsif request.headers['src']
+        access_token = request.headers['access-token']
+        uid = request.headers['uid']
+        client = request.headers['client']
+        if request.headers['src'] == 'patient'
           self.current_patient = find_verified_patient(access_token, uid, client)
           logger.add_tags 'ActionCable', current_patient.cpf
-        elsif request.headers["src"] == "medic"
+        elsif request.headers['src'] == 'medic'
           self.current_medic = find_verified_medic(access_token, uid, client)
           logger.add_tags 'ActionCable', current_medic.cpf
         end
-      end      
+      end
     end
 
     private
 
-    def find_verified_admin admin_id
+    def find_verified_admin(admin_id)
       if user = Admin.find_by(id: admin_id)
-          user
+        user
       else
-          reject_unauthorized_connection
+        reject_unauthorized_connection
       end
     end
-
   end
 end
--- config/initializers/warden_hooks.rb
+++ config/initializers/warden_hooks.rb
@@ -1,11 +1,11 @@
-Warden::Manager.after_set_user do |user,auth,opts|
-    scope = opts[:scope]
-    #auth.cookies.signed["#{scope}.id"] = user.id
-    auth.cookies.signed["#{scope}_id"] = user.id
+Warden::Manager.after_set_user do |user, auth, opts|
+  scope = opts[:scope]
+  # auth.cookies.signed["#{scope}.id"] = user.id
+  auth.cookies.signed["#{scope}_id"] = user.id
 end
 
-Warden::Manager.before_logout do |user, auth, opts|
-    scope = opts[:scope]
-    #auth.cookies.signed["#{scope}.id"] = nil
-    auth.cookies.signed["#{scope}_id"] = nil
+Warden::Manager.before_logout do |_user, auth, opts|
+  scope = opts[:scope]
+  # auth.cookies.signed["#{scope}.id"] = nil
+  auth.cookies.signed["#{scope}_id"] = nil
 end
